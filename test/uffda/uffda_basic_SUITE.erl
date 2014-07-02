04cd0140fa6f26a2bd9e11fd045d8fc76a424be9
Tim Holland <th0114nd@gmail.com>
2014-07-02 10:42:04 -0700

    Changed application shutdown behavior to actually run properly.

--- test/uffda/uffda_basic_SUITE.erl
+++ test/uffda/uffda_basic_SUITE.erl
@@ -7,9 +7,15 @@
         easy/1
         ]).
 
+-export([
+    create_service/1,
+    service_loop/1
+    ]).
+
 -include("uffda_common_test.hrl").
 
 all() -> [
+    easy,
     easy
     ].
 
@@ -17,20 +23,14 @@ init_per_suite(Config) -> Config.
 end_per_suite(Config) -> Config.
 
 init_per_testcase(_TestCase, Config) ->
-    Out = uffda:start(),
-    case Out of
-        ok -> Config;
-        _ -> error_logger:error_msg("Out is ~p~n", [Out])
-    end.
+    ok = uffda:start(),
+    Config.
 
 end_per_testcase(_TestCase, _Config) ->
-    Pid = whereis(uffda),
-    unlink(Pid),
     uffda:stop().
 
 -spec easy(term()) -> ok.
 easy(_Config) -> 
-    ct:log("okay"),
     {ok, _Pid} = sr_client:register_me(foo),
     'STARTING_UP' = sr_client:get_state(foo),
     ok = sr_client:go_up(foo),
@@ -38,6 +38,21 @@ easy(_Config) ->
     ok = sr_client:go_down(foo),
     'DOWN' = sr_client:get_state(foo),
     ok = sr_client:reset(foo),
-    'STARING_UP' = sr_client:get_state(foo),
-    ct:log("finished up"),
-    true.
+    'STARTING_UP' = sr_client:get_state(foo),
+    ok.
+
+-spec create_service(atom()) -> pid().
+create_service(Name) -> spawn(?MODULE, fun service_loop/1, [Name]).
+
+
+-spec service_loop(atom()) -> term().
+service_loop(Name) ->
+    receive
+        die -> exit(told_to);
+        reg -> sr_client:register_me(Name);
+        up -> sr_client:go_up(Name);
+        down -> sr_client:go_down(Name);
+        reset -> sr_client:reset(Name);
+        {state, Pid} -> Pid ! sr_client:get_state(Name)
+    end,
+    service_loop(Name).
