b06552fd630eb6582ec97a4a613c52d085c30d8c
Tim Holland <th0114nd@gmail.com>
2014-07-02 14:18:10 -0700

    Added basic skeleton for proper testing.

---
+++ test/uffda/uffda_proper_SUITE.erl
@@ -0,0 +1,14 @@
+-module(uffda_proper_SUITE).
+-vsn('').
+
+-export([all/0, init_per_suite/1, end_per_suite/1]).
+
+-export([basic/1]).
+
+all() -> [basic].
+
+init_per_suite(Config) -> Config.
+end_per_suite(Config) -> Config.
+
+%% Testcases.
+basic() -> ok.
