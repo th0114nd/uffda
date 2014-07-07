8a479bd1f05101a2c721bc26fd8fe83eb3587e3a
Jay Nelson <jay@duomark.com>
2014-07-07 13:12:11 -0700

    Enhance FSM with registered, restarting and crashed states

--- src/uffda_client.erl
+++ src/uffda_client.erl
@@ -3,7 +3,6 @@
 -export([
          register_service/1,
          register_service/2,
-         reset_service/1,
          unregister_service/1,
          starting_service/1,
          set_service_online/1,
@@ -37,12 +36,12 @@ register_service(Service_Name, Service_Pid)
     case uffda_registry_sup:start_child(Service_Name, Service_Pid) of
         {ok, _Fsm_Pid} -> ok;
         {error, {already_started, Fsm_Pid}} ->
-            gen_fsm:sync_send_all_state_event(Fsm_Pid, {re_init, Service_Pid})
+            trigger_all_event(Fsm_Pid, {re_init, Service_Pid})
     end.
 
 unregister_service(Service_Name)
   when is_atom(Service_Name) ->
-    Fsm_Name = uffda_service_fsm:fsm_name_from_service(Service_Name),
+    Fsm_Name = uffda_service_fsm:existing_fsm_name_from_service(Service_Name),
     Fsm_Pid = whereis(Fsm_Name),
     uffda_registry_sup:stop_child(Fsm_Pid).
 
@@ -51,22 +50,22 @@ unregister_service(Service_Name)
 -type event_response()  :: {error, {not_registered, service_name()}} | ok.
 -type status_response() :: {error, {not_registered, service_name()}} | service_status().
 
--spec reset_service       (service_name()) -> event_response().
 -spec starting_service    (service_name()) -> event_response().
 -spec set_service_online  (service_name()) -> event_response().
 -spec set_service_offline (service_name()) -> event_response().
 -spec service_status      (service_name()) -> status_response().
     
-starting_service    (Service_Name) -> trigger_event(Service_Name, starting,       async).
-set_service_online  (Service_Name) -> trigger_event(Service_Name, online,         async).
-set_service_offline (Service_Name) -> trigger_event(Service_Name, offline,        async).
-reset_service       (Service_Name) -> trigger_event(Service_Name, reset,          async).
-service_status      (Service_Name) -> trigger_event(Service_Name, current_status, sync).
+starting_service    (Service_Name) -> trigger_event     (Service_Name, starting).
+set_service_online  (Service_Name) -> trigger_event     (Service_Name, online).
+set_service_offline (Service_Name) -> trigger_event     (Service_Name, offline).
+service_status      (Service_Name) -> trigger_all_event (Service_Name, current_status).
 
-trigger_event(Service_Name, Service_Event, Sync_Or_Async)
-  when is_atom(Service_Name) ->
-    Fsm_Name = uffda_service_fsm:fsm_name_from_service(Service_Name),
-    case Sync_Or_Async of
-        sync  -> gen_fsm:sync_send_all_state_event(Fsm_Name, Service_Event);
-        async -> gen_fsm:send_event(Fsm_Name, Service_Event)
-    end.
+trigger_event(Service_Name, Service_Event)  when is_atom(Service_Name) ->
+    Fsm_Name = uffda_service_fsm:existing_fsm_name_from_service(Service_Name),
+    gen_fsm:send_event(Fsm_Name, Service_Event).
+
+trigger_all_event(Service_Pid,  Service_Event) when is_pid(Service_Pid) ->
+    gen_fsm:sync_send_all_state_event(Service_Pid, Service_Event);
+trigger_all_event(Service_Name, Service_Event) when is_atom(Service_Name) ->
+    Fsm_Name = uffda_service_fsm:existing_fsm_name_from_service(Service_Name),
+    gen_fsm:sync_send_all_state_event(Fsm_Name, Service_Event).
