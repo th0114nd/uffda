edca8ee92bfa7dad9eb9eb5817b07cca8add79f5
Tim Holland <th0114nd@gmail.com>
2014-07-07 16:21:59 -0700

    First brush at creating a behavior to specify the service interface

---
+++ src/uffda_service.erl
@@ -0,0 +1,16 @@
+-module(uffda_service).
+-export([start/4, stop/2]).
+
+-callback init(Args :: list(term())) -> ok | {error, Reason :: term()}.
+-callback mainloop(Args :: list(term())) -> ok | {error, Reason :: term()}.
+-callback terminate() -> ok | {error, Reason :: term()}.
+
+start(Service_Name, Service_Module, Init_Args, Loop_Args) ->
+    uffda_client:register_service(Service_Name),
+    {ok, Pid} = apply(Service_Module, init, Init_Args),
+    uffda_client:starting_up(Service_Module, Pid),
+    apply(Service_Module, mainloop, Loop_Args).
+   
+stop(Service_Name, Service_Module) ->
+    ok = apply(Service_Module, terminate, []),
+    uffda_client:unregister_service(Service_Name).
