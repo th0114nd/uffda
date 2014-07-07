1afe012844f384c433991ee206bf4f9ee0c5426b
Tim Holland <th0114nd@gmail.com>
2014-07-07 16:55:32 -0700

    Modifications for simplicity and performance.

--- src/uffda_service.erl
+++ src/uffda_service.erl
@@ -7,10 +7,10 @@
 
 start(Service_Name, Service_Module, Init_Args, Loop_Args) ->
     uffda_client:register_service(Service_Name),
-    {ok, Pid} = apply(Service_Module, init, Init_Args),
+    {ok, Pid} = Service_Module:init(Init_Args),
     uffda_client:starting_up(Service_Module, Pid),
-    apply(Service_Module, mainloop, Loop_Args).
+    Service_Module:mainloop(Loop_Args).
    
 stop(Service_Name, Service_Module) ->
-    ok = apply(Service_Module, terminate, []),
+    ok = Service_Module:terminate(),
     uffda_client:unregister_service(Service_Name).
