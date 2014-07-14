30d06856eb614e0bb1cd762d78b8b2bb8a240733
Tim Holland <th0114nd@gmail.com>
2014-07-14 09:40:48 -0700

    Started working on typing the DSL

---
+++ src/uffda_dsl.erl
@@ -0,0 +1,14 @@
+-module(uffda_dsl).
+
+-include("uffda.hrl").
+
+-type program() :: {sup_tree_spec(), [{service_name(), service_event()}]}.
+-type sup_tree_spec() :: indiv_spec() | {indiv_spec(), [sup_tree_spec()]}.
+-type indiv_spec() :: [child_desc()].
+-type child_desc() :: {{any(), mfargs(), restart(), shutdown(), workorsup(), modules()}, service_status()}.
+
+-type mfargs() :: {atom(), atom(), [any()]}.
+-type restart() :: permanent | transient | temporary.
+-type shutdown() :: brutal_kill | pos_integer() | infinity.
+-type workorsup() :: worker | supervisor.
+-type modules() :: [atom()] | dynamic.
