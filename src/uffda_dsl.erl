bb3b6fd559e5918c26e2bc9051ec21e51eef9ffa
Tim Holland <th0114nd@gmail.com>
2014-07-14 13:54:46 -0700

    Reworked the tree structure to be more homogeneous

--- src/uffda_dsl.erl
+++ src/uffda_dsl.erl
@@ -4,17 +4,13 @@
 
 -export([parse_from_file/1, run_program/1]).
 -type program() :: {sup_tree_spec(), [action()]}.
--type sup_tree_spec() :: indiv_spec() | {indiv_spec(), [sup_tree_spec()]}.
--type indiv_spec() :: {indiv, module(), term(), [child_desc()]}.
--type child_desc() :: {{any(), mfargs(), restart(), shutdown(), workorsup(), modules()}, service_status()}.
+-type sup_tree_spec() :: wos() | {wos(), [wos()]}.
+-type wos() :: {worker, worker_desc()} | {supervisor, super_desc()}.
+% In general, {atom(), Module, Args}
+-type super_desc() :: {atom(), ex_super, term()}.
+-type worker_desc() :: {atom(), ex_worker, service_status()}.
 -type action() :: {service_name(), service_event()}.
 
--type mfargs() :: {atom(), atom(), [any()]}.
--type restart() :: permanent | transient | temporary.
--type shutdown() :: brutal_kill | pos_integer() | infinity.
--type workorsup() :: worker | supervisor.
--type module() :: atom().
--type modules() :: [module()] | dynamic.
 -type reason() :: term().
 
 -type deep_list() :: [char() | atom() | deep_list()].
@@ -44,7 +40,7 @@ create_sup_tree({Node, Below}) ->
 
 -spec ensure_workers_valid(sup_tree_spec(), [action()]) -> boolean(). 
 ensure_workers_valid(_Sup_Tree, _Actions) ->
-    % Wait until further functionality added.
+    % Wait until further functionality added to implement.
     true.
 
 -spec execute_all([action()]) -> ok.
