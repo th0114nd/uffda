9011b31aa1f40a4875d0f016f4b494cdc78321b1
Pierce <pierce.mt92@gmail.com>
2014-07-15 10:12:07 -0700

    Functioning tree processing.

--- src/tree_processing.erl
+++ src/tree_processing.erl
@@ -1,30 +1,36 @@
 -module(tree_processing).
--export([translate_tree/1]).
+-export([read_and_translate/1]).
 
+%% Extracts supervision tree and events list from uffda_dsl:parse_from_file.
+read_and_translate(FileRead) ->
+    {ok, {{startup, Tree}, {actions, Events}}} = FileRead,
+    translate_tree({Tree, Events}).
 
 %% Symbolically calculates final worker states.
 -spec state_change(atom(), term()) -> atom().
 state_change(_, online) -> up;
 state_change(_, offline) -> down;
-%% Worry about initial states for STARTING/RESTARTING???
 state_change(_, {starting, _}) -> starting_up;
 state_change(_, {re_init, _}) -> restarting;
 state_change(_, unregister) -> killed;
-state_change(killed, _) -> killed.
+state_change(killed, _) -> killed;
+state_change(State, _) -> State.
 
+%% Applies state_change to leaf node or evaluates subtree.
 -spec transition(uffda_dsl:program()) -> uffda_dsl:program() | ok.
 transition({{leaf, Leaf}, Events}) ->
     {worker, {Name, ex_worker, Status}} = Leaf,
-    Actions = proplist:get_all_values(Name, Events),
+    Actions = proplists:get_all_values(Name, Events),
     FinalState = lists:foldl(fun(Event, Acc) -> state_change(Acc, Event) end,
-                Status, Events),
-    {{worker, {Name, ex_worker, FinalState}}, Events};
+                Status, Actions),
+    {leaf, {worker, {Name, ex_worker, FinalState}}};
 transition({{node, Parent, Children}, Events}) ->
     translate_tree({{node, Parent, Children}, Events}).
 
+%% Evaluates Events for immediate children of the root of Tree.
 -spec translate_tree(uffda_dsl:program()) -> uffda_dsl:program() | ok.
 translate_tree({Tree, Events}) -> 
     {node, Parent, Children} = Tree,
-    NewTree = lists:map(fun(Child) -> ?MODULE:transition({Child, Events}) end, Children),
+    NewTree = {node, Parent, lists:map(fun(Child) -> transition({Child, Events}) end, Children)},
     {NewTree, Events}.
  
