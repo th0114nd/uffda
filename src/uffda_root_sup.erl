7a105d235c90d950cd649cd89448c5346007b108
Tim Holland <th0114nd@gmail.com>
2014-07-16 10:38:27 -0700

    Cleaned up the application start interface to allow a list of things to enable.

--- src/uffda_root_sup.erl
+++ src/uffda_root_sup.erl
@@ -2,28 +2,29 @@
 -behavior(supervisor).
 
 -export([start_link/1, init/1, run_prog/0]).
-
+-export([service_registry/0, prog_tree_test/0]).
 -define(SUPER, ?MODULE).
 -define(CHILD(__Mod, __Args), {__Mod, {__Mod, start_link, __Args},
                                permanent, 2000, worker, [__Mod]}).
--spec start_link(Include_Testing :: boolean()) -> {ok, pid()}.
-start_link(IT) 
-  when is_boolean(IT) ->
-    supervisor:start_link({local, ?SUPER}, ?MODULE, IT).
+-spec start_link([atom()]) -> {ok, pid()}.
+start_link(Enabled) 
+  when is_list(Enabled) ->
+    supervisor:start_link({local, ?SUPER}, ?MODULE, Enabled).
 
 -spec init(Include_Testing :: boolean()) -> {ok, {{supervisor:strategy(), non_neg_integer(), non_neg_integer()},
                                                   [supervisor:child_specs()]}}.
-init(true) ->
-    Procs = [{uffda_sup_sup, {?MODULE, run_prog, []}, transient, 2000, worker, [uffda_sup_sup]}],
-    init_act(Procs);
-init(false) -> 
-    init_act([]).
+init(Enabled) 
+  when is_list(Enabled) ->
+    Procs = [?MODULE:Enable() || Enable <- Enabled],
+    {ok, {{one_for_one, 10, 10}, Procs}}.
 
--spec init_act([supervisor:child_specs()]) -> {ok, {{supervisor:strategy(), non_neg_integer(), non_neg_integer()},
-                                                    [supervisor:child_specs()]}}.
-init_act(Procs) ->
-    NewProcs = [?CHILD(uffda_registry_sup, []) | Procs],
-    {ok, {{one_for_one, 10, 10}, NewProcs}}.
+-spec service_registry() -> supervisor:childspec().
+service_registry() ->
+    ?CHILD(uffda_registry_sup, []).
+
+-spec prog_tree_test() -> supervisor:childspec().
+prog_tree_test() ->
+    {?MODULE, {?MODULE, run_prog, []}, transient, 2000, worker, [?MODULE]}.
 
 -spec run_prog() -> {ok, pid()}.
 run_prog() ->
