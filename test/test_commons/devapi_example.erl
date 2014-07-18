6a6997af0f7bbc043c0c770393120f2c37f7f9cf
Pierce <pierce.mt92@gmail.com>
2014-07-18 10:31:57 -0700

    Added deduce.

--- test/test_commons/devapi_example.erl
+++ test/test_commons/devapi_example.erl
@@ -5,7 +5,7 @@
          get_all_test_model_ids/0,
          generate_proper_model/2,
          deduce_proper_expected_status/1,
-
+         deduce/3,
          vivify_proper_scenario/1,
          translate_proper_scenario_dsl/1,
          translate_proper_scenario_events/1,
@@ -22,7 +22,10 @@ get_all_test_model_ids() ->
 -spec generate_proper_model(Model_Id :: tc_proper_model_id(), Source :: tc_proper_model_source()) -> tc_proper_model().
 generate_proper_model(Id, {file, Filename} = Source) ->
     {ok, Scenarios} = file:consult(Filename),
-    #tc_proper_model{id=Id, source=Source, behaviour=?MODULE, scenarios=Scenarios}.
+    Pairs = lists:zip(lists:seq(1, length(Scenarios)), Scenarios),
+    TCScenarios = [#tc_proper_scenario{instance=SID, scenario_desc=S, initial_status=[], events=[register]}
+                   || {SID, S} <- Pairs], 
+    #tc_proper_model{id=Id, source=Source, behaviour=?MODULE, scenarios=TCScenarios}.
 
 -spec deduce_proper_expected_status(Scenario_Instance :: tc_proper_scenario()) -> Expected_Status :: term().
 deduce_proper_expected_status(#tc_proper_scenario{} = Scenario) ->
@@ -36,14 +39,14 @@ deduce_proper_expected_status(#tc_proper_scenario{} = Scenario) ->
 
 -spec vivify_proper_scenario(Scenario :: tc_proper_scenario()) -> tc_proper_scenario_live_ref().
 vivify_proper_scenario(#tc_proper_scenario{} = _Scenario) ->
-    devapi_tester.
+    uffda_registry_sup.
 
 -spec translate_proper_scenario_dsl(tc_proper_scenario_dsl_desc()) -> tc_proper_scenario_live_desc().
-translate_proper_scenario_dsl(Dsl_Scenario) ->
+translate_proper_scenario_dsl(Dsl_Scenario) when is_atom(Dsl_Scenario) ->
     Dsl_Scenario.
 
 -spec translate_proper_scenario_events(tc_proper_scenario_dsl_events()) -> tc_proper_scenario_live_events().
-translate_proper_scenario_events(Dsl_Events) ->
+translate_proper_scenario_events(Dsl_Events) when is_atom(Dsl_Events) ->
     Dsl_Events.
 
 -spec generate_proper_observation(tc_proper_scenario_live_ref(), tc_proper_test_case()) -> term().
@@ -60,5 +63,12 @@ passed_proper_test_case(_Case_Number, _Expected_Status, _Observed_Status) ->
 %%--------------------------------
 %% Support functions
 %%--------------------------------
-deduce(_Desc, _Init_Status, _Events) ->
-    expected.
+deduce_event([Service], register) when is_atom(Service) -> {ok, [Service]};
+deduce_event([Service], unregister) when is_atom(Service) -> {ok, []};
+deduce_event(Service, which_services)  -> {Service, Service}. 
+deduce(Desc, _Init_Status, Events) ->
+    {_, Output} = lists:foldl(fun(Event, {RegisteredService, Results}) ->
+                    {Return, NewlyRegistered} = deduce_event(RegisteredService, Event), 
+                    {NewlyRegistered, [Return|Results]}
+                    end, {[Desc], []}, Events), 
+    lists:reverse(Output).
