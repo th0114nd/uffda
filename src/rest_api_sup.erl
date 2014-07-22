a56c4f0faf8357bf9d2fe4f6893deaea03cc6243
Tim Holland <th0114nd@gmail.com>
2014-07-22 14:14:25 -0700

    Hello world of cowboy

--- src/rest_api_sup.erl
+++ src/rest_api_sup.erl
@@ -1,9 +1,30 @@
 -module(rest_api_sup).
--export([start_link/0, init/1]).
 
--spec start_link() -> {ok, pid()}.
+-behavior(supervisor).
+
+-export([start_link/0, init/1, listen/0]).
+
+-spec init(term()) -> {ok, {{supervisor:strategy(), non_neg_integer(), non_neg_integer()},
+                            [supervisor:child_specs()]}}.
+-define(SUPER(__Mod, __Args), {__Mod, {__Mod, start_link, __Args}, permanent, infinity, supervisor, [__Mod]}).
+-define(SERVER, ?MODULE).
+
 start_link() ->
-    supervisor:start_link({local, ?MODULE}, ?MODULE, []).
+    supervisor:start_link({local, ?SERVER}, ?MODULE, {}).
 
-init([]) ->
-    {ok, {{one_for_one, 10, 10}, []}}.    
+init(_Args) ->
+    Ranch_Sup = ?SUPER(ranch_sup, []),
+    Cowboy_Sup = ?SUPER(cowboy_sup, []),
+    Begin_Listen = {?MODULE,  {?MODULE, listen, []}, transient, infinity, worker, [?MODULE]},
+    Included_Apps = [Ranch_Sup, Cowboy_Sup, Begin_Listen],
+    {ok, {{rest_for_one, 5, 60}, Included_Apps}}.
+   
+   
+listen() ->
+    Dispatch = cowboy_router:compile([
+        {'_', [
+            {"/", rest_handler, []}
+            ]}
+        ]),
+    {ok, _} = cowboy:start_http(http, 10, [{port, 8000}], [
+        {env, [{dispatch, Dispatch}]}]). 
--- src/rest_handler.erl
+++ src/rest_handler.erl
@@ -2,11 +2,22 @@
 
 %% Cowboy REST callbacks
 -export([init/3,
-         allowed_methods/2]).
+         allowed_methods/2,
+         handle/2,
+         terminate/3]).
 
-init(_Transport, _Req, _Opts) ->
-    {upgrade, protocol, cowboy_rest}.
+init(_Transport, Req, _Opts) ->
+    {ok, Req, undefined}.
+%    {upgrade, protocol, cowboy_rest}.
 
 allowed_methods(Req, State) ->
     {[<<"GET">>, <<"PUT">>], Req, State}.
 
+handle(Req, State) ->
+    {ok, Req2} = cowboy_req:reply(200, [
+        {<<"content-type">>, <<"text/plain">>}
+    ], <<"rest hello world">>, Req),
+    {ok, Req2, State}.
+
+terminate(_Reason, _Req, _State) ->
+    ok.
