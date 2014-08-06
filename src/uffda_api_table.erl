913980f0acf066ddeca3c16b00ffcebae0c07778
Pierce <pierce.mt92@gmail.com>
2014-08-06 15:07:11 -0700

    Added GET functionality, including 404 for unregistered services.

--- src/uffda_api_table.erl
+++ src/uffda_api_table.erl
@@ -5,7 +5,9 @@
 listen() ->
     Dispatch = cowboy_router:compile([
         {'_', [
-            {<<"/uffda_service/[:name]">>, rest_handler, []}
+            {<<"/uffda-services">>, uffda_rest_handler, [which_services]},
+            {<<"/uffda-services/hello">>, uffda_rest_handler, [hello_to_text]},
+            {<<"/uffda-services/:name">>, uffda_rest_handler, [service_status]}
             ]}
         ]),
     {ok, _} = cowboy:start_http(http, 10, [{port, 8000}], [
--- src/uffda_rest_handler.erl
+++ src/uffda_rest_handler.erl
@@ -2,32 +2,58 @@
 
 %% Cowboy REST callbacks
 -export([init/3,
+         rest_init/2,
          allowed_methods/2,
+         resource_exists/2,
          content_types_provided/2,
-         handle/2,
          terminate/3]).
 
--export([hello_to_text/2]).
+%% Calls to uffda
+-export([which_services/2,
+         service_status/2,
+         hello_to_text/2
+        ]).
+
+-record(state,
+        {fn :: atom()}).
 
 init(_Transport, _Req, _Opts) ->
     {upgrade, protocol, cowboy_rest}.
 
+rest_init(Req, [Function]) ->
+    {ok, Req, #state{fn = Function}}.
+
 allowed_methods(Req, State) ->
-    {[<<"GET">>, <<"PUT">>, <<"POST">>], Req, State}.
+    {[<<"GET">>], Req, State}.
+
+resource_exists(Req, State) ->
+    {Service_Bin, Req2} = cowboy_req:binding(name, Req),
+    Status = 
+        case Service_Bin of 
+            undefined -> undefined;
+            _ -> uffda_client:service_status(binary_to_atom(Service_Bin, latin1))
+        end,
+    case Status of
+        {error, _} -> {false, Req2, State};
+        _ -> {true, Req2, State}
+    end.
 
 content_types_provided(Req, State) ->
     {[
-        {<<"text/plain">>, hello_to_text}
+        {<<"text/plain">>, State#state.fn}
     ], Req, State}.
 
-handle(Req, State) ->
-    {ok, Req2} = cowboy_req:reply(200, [
-        {<<"content-type">>, <<"text/plain">>}
-    ], <<"rest hello world">>, Req),
-    {ok, Req2, State}.
+which_services(Req, State) ->
+    IO_Services_List = [atom_to_list(Service) ++ "\n" || Service <- uffda_client:which_services()],
+    {list_to_binary(IO_Services_List), Req, State}.
+
+service_status(Req, State) ->
+    {Service_Bin, Req2} = cowboy_req:binding(name, Req),
+    Service_Name = binary_to_atom(Service_Bin, latin1),
+    {list_to_binary(atom_to_list(uffda_client:service_status(Service_Name)) ++ "\n"), Req2, State}.
 
 hello_to_text(Req, State) ->
-    {<<"Rest hello world">>, Req, State}.
+    {<<"Hello World!\n">>, Req, State}.
 
 terminate(_Reason, _Req, _State) ->
     ok.
