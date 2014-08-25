d09934d47c2a18ecac795ccd1d3df9c63e545904
Tim Holland <th0114nd@gmail.com>
2014-08-25 15:53:15 -0700

    Un-deleted needed test files

---
+++ test/test_commons/ex_super.erl
@@ -0,0 +1,13 @@
+-module(ex_super).
+-behavior(supervisor).
+
+-export([start_link/1, init/1]).
+
+-spec start_link(atom()) -> {ok, pid()}.
+start_link(Name) ->
+    supervisor:start_link({local, Name}, ?MODULE, {}).
+    
+-spec init({}) -> {ok, {{supervisor:strategy(), non_neg_integer(),non_neg_integer()},
+                        [supervisor:child_spec()]}}.  
+init({}) ->
+    {ok, {{one_for_one, 10, 10}, []}}.
