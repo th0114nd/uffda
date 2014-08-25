c82fe99413b6de450c4627d2964757296883c107
Tim Holland <th0114nd@gmail.com>
2014-08-25 15:54:01 -0700

    Changed to using start_phases, and properly shutting things down.

--- src/uffda_api_sup.erl
+++ src/uffda_api_sup.erl
@@ -15,7 +15,6 @@ start_link() ->
 init(_Args) ->
     Ranch_Sup = ?SUPER(ranch_sup, []),
     Cowboy_Sup = ?SUPER(cowboy_sup, []),
-    Begin_Listen = {uffda_api_table,  {uffda_api_table, listen, []}, transient, infinity, worker, [uffda_api_table]},
 	Sys_Sup = ?SUPER(sys_sup, []),
-    Apps_N_Children = [Ranch_Sup, Cowboy_Sup, Begin_Listen, Sys_Sup],
-    {ok, {{rest_for_one, 5, 60}, Apps_N_Children}}.
+    Apps = [Ranch_Sup, Cowboy_Sup, Sys_Sup],
+    {ok, {{rest_for_one, 5, 60}, Apps}}.
