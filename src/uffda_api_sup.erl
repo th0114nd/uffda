218f5783d0f8fd46a5dbdfea7f8c4ab32ac1cf0d
Tim Holland <th0114nd@gmail.com>
2014-08-26 13:43:16 -0700

    fixed plt error

--- src/uffda_api_sup.erl
+++ src/uffda_api_sup.erl
@@ -11,7 +11,7 @@ start_link() ->
     supervisor:start_link({local, ?SERVER}, ?MODULE, {}).
 
 -type restart() :: {supervisor:strategy(), non_neg_integer(), non_neg_integer()}.
--spec init({}) -> {'ok',{{'rest_for_one',non_neg_integer(),non_neg_integer()}, [?SUPER(module())]}}.
+-spec init({}) -> {'ok',{restart(), [?SUPER(module())]}}.
 init({}) ->
     Ranch_Sup = ?SUPER(ranch_sup),
     Cowboy_Sup = ?SUPER(cowboy_sup),
