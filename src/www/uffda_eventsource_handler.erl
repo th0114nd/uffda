929b0716e57802f18f3b69d0f2f6a6efe440dc46
Tim Holland <th0114nd@gmail.com>
2014-08-28 10:05:22 -0700

    Fixed function clause bug

--- src/www/uffda_eventsource_handler.erl
+++ src/www/uffda_eventsource_handler.erl
@@ -8,17 +8,17 @@ init(_Transport, Req, []) ->
     {{Name, Status}, Req2} = cowboy_req:binding(name, Req),
     Headers = [{<<"content-type">>, <<"text/event-stream">>}],
     {ok, Req3} = cowboy_req:chunked_reply(200, Headers, Req2),
-    ok = cowboy_req:chunk(["event: subscription-start\n",
-                      "id: ", atom_to_list(Name), "\n",
-                      "data: ", atom_to_list(Status), "\n\n"], Req3),
     uffda_subscription:subscribe(sse, self(), Name, Status), 
     {loop, Req3, Name}.
 
-info({update, {Name, Status}}, Req, Name) ->
-    ok = cowboy_req:chunk(["event: update\n",
+info({Event, {Name, Status}}, Req, Name) ->
+    ok = cowboy_req:chunk(["event: ", atom_to_list(Event), "\n",
                            "id: ", atom_to_list(Name), "\n",
                            "data: ", atom_to_list(Status), "\n\n"], Req),
-    {loop, Req, Name}.
+    case Event of
+        Ev when Ev /= unsubscribing -> {loop, Req, Name};
+        unsubscribing -> {ok, Req, Name}
+    end.
 
 terminate(_Reason, _Req, _State) ->
     ok.
